63283203d1c8523348bcf400122661185611a6d5
seydadilandursun <108757360+seydadilandursun@users.noreply.github.com>
2022-07-22 23:00:07 +0300

    hata yonetimi eklendi

---
+++ hatayonetimi/Program.cs
@@ -0,0 +1,41 @@
+﻿// Hata Yonetimi
+//try
+//{
+//Console.WriteLine("Bir sayi giriniz");
+//int sayi=Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Girmis oldugunuz sayi sudur:"+ sayi);
+//}
+//catch(Exception ex){
+//    Console.WriteLine("Hata"+ex.Message);
+//}
+//finally
+//{
+//    Console.Write("islem tamamlandi");
+//}
+
+try
+{
+    //int a=int.Parse(null);
+    // int a=int.Parse("test");
+    int a=int.Parse("569409809978");
+
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine("Bos arguman  girdiniz.");
+    Console.WriteLine(ex);
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("Veri tipi uygun degil.");
+    Console.WriteLine(ex);
+}
+catch (OverflowException ex)
+{
+    Console.WriteLine("Cok buyuk ya da cok buyuk bir deger girdiniz.");
+    Console.WriteLine(ex);
+}
+   finally
+   {
+    Console.Write("islem tamamlandi");
+   }
